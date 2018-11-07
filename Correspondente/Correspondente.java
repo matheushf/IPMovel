d0b56a1eecbc37e595ec284e296c2e57836729ca
matheushf <hffmatheus@gmail.com>
2018-11-07 19:42:45 -0200

    ajustes

--- Correspondente/Correspondente.java
+++ Correspondente/Correspondente.java
@@ -1,11 +1,13 @@
 package Correspondente;
 
 import AgenteMovel.AgenteMovel;
+import client.Mensagem;
 import Correspondente.CorrespondenteConstant;
 
 public class Correspondente {
 
   AgenteMovel agenteMovel = new AgenteMovel();
+  Mensagem mensagem  = new Mensagem("OI", "172.16.0.90", "172.16.0.92");
 
   public void main(String args[]) {
   }
--- Mensagem/Mensagem.java
+++ Mensagem/Mensagem.java
@@ -5,10 +5,15 @@ import Correspondente.Correspondente;
 public class Mensagem {
 	
 	public String mensagem = "Alguma mensagem";
+	public String ipRemetente = "0.0.0.0";
+	public String ipDestinatario = "0.0.0.0";
 
-	Correspondente correspondente = new Correspondente();
+	public Mensagem(String mensagem, String ipRemetente, String ipDestinatario) {
+		this.mensagem = mensagem;
+		this.ipRemetente = ipRemetente;
+		this.ipDestinatario = ipDestinatario;
+	}
 
 	public void main(String args[]) {
-		correspondente.transmitirMensagem(mensagem);
-	}
+	}	
 }
