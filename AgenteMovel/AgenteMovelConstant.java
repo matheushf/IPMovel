75d55ce1136882bf2a4f2992dd3a601b090fbd39
matheushf <hffmatheus@gmail.com>
2018-12-06 15:07:59 -0200

    agente movel removido

--- AgenteMovel/AgenteMovelConstant.java
+++
@@ -1,8 +0,0 @@
-package AgenteMovel;
-
-public class AgenteMovelConstant {
-	
-	public static final String RMI_ID = "AgenteMovelRMI";
-	public static final int RMI_PORT = 1099;
-
-}
--- Correspondente/Correspondente.java
+++ Correspondente/Correspondente.java
@@ -5,13 +5,11 @@ import java.rmi.registry.Registry;
 
 import AgenteHome.AgenteHomeConstant;
 import AgenteHome.AgenteHomeInterface;
-import AgenteMovel.AgenteMovel;
 import Mensagem.Mensagem;
 import Correspondente.CorrespondenteConstant;
 
 public class Correspondente {
 
-  public static AgenteMovel agenteMovel = new AgenteMovel();
   public static Mensagem mensagem = new Mensagem("OI", "172.16.0.83", "172.16.0.81", "172.16.0.82");
 
   public static void main(String args[]) {
@@ -20,7 +18,6 @@ public class Correspondente {
 
   public static void transmitirMensagem(Mensagem mensagem) {
     System.out.println("Correspondente transmitirMensagem para HA: " + mensagem.coa + " NoMovel: " + mensagem.ipDestinatario);
-    // agenteMovel.enviaMensagem(mensagem);
 
     Correspondente correspondente = new Correspondente();
     AgenteHomeInterface homeAgent = correspondente.conectaHomeAgent(mensagem.coa);
